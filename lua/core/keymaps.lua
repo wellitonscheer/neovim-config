9aac8d323a4c787cb578cfee49e3c990f9478465
wellitonscheer <wellitonscheer@gmail.com>
2023-08-09 17:56:11 -0300

    config from nvim-lua/kickstart.nvim

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,9 +1,9 @@
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 vim.opt.termguicolors = true
 
-vim.g.mapleader = " "
-vim.opt.autowrite = true
 vim.opt.clipboard = "unnamedplus"
 
 vim.o.tabstop = 2           -- Number of spaces tabs count for
@@ -12,3 +12,39 @@ vim.o.softtabstop = 2       -- Number of spaces for backspace/delete operations
 vim.o.expandtab = true      -- Convert tabs to spaces
 vim.o.smartindent = true    -- Enable smart indentation
 vim.o.autoindent = true     -- Enable automatic indentation
+
+vim.o.hlsearch = false
+
+-- Make line numbers default
+vim.wo.number = true
+
+-- Enable mouse mode
+vim.o.mouse = 'a'
+
+-- Sync clipboard between OS and Neovim.
+--  Remove this option if you want your OS clipboard to remain independent.
+--  See `:help 'clipboard'`
+--vim.o.clipboard = 'unnamedplus'
+
+-- Enable break indent
+vim.o.breakindent = true
+
+-- Save undo history
+vim.o.undofile = true
+
+-- Case-insensitive searching UNLESS \C or capital in search
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+-- Keep signcolumn on by default
+vim.wo.signcolumn = 'yes'
+
+-- Decrease update time
+vim.o.updatetime = 250
+vim.o.timeoutlen = 300
+
+-- Set completeopt to have a better completion experience
+vim.o.completeopt = 'menuone,noselect'
+
+-- NOTE: You should make sure your terminal supports this
+--vim.o.termguicolors = true
